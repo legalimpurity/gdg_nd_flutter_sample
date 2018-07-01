a0d75715c677f1f78675e91d8547d4e9094984cc
rajatkhanna <legalimpurity@gmail.com>
2018-07-01 13:47:27 +0530

    sessions progress.

--- lib/main.dart
+++ lib/main.dart
@@ -1,109 +1,115 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(new MyApp());
+void main() {
+  runApp(ShoppingHomePage());
+}
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class ShoppingHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
+    return MaterialApp(
+      color: Colors.brown,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Simple List App"),
+        ),
+        body: ListWidget(),
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: true,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class ListWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return new Scaffold(
-      appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title),
+    return Container(
+      margin: EdgeInsets.all(8.0),
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.black,
+          width: 20.0
+        )
       ),
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
+      child: ListItemStack(),
+    );
+  }
+}
+
+class ListItemStack extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      fit: StackFit.expand,
+      children: <Widget>[
+        imageItem(),
+        nameItem(),
+        ratingItem(),
+      ],
+    );
+  }
+}
+
+// Fuctions
+Widget imageItem() => Container(
+    color: Colors.pink,
+  child: Image.network("https://assets.abfrlcdn.com/img/app/product/1/199775-614183-large.jpg",
+  fit: BoxFit.cover,),
+  );
+
+Widget nameItem() =>
+    Align(
+      alignment: Alignment.bottomCenter,
+      child: Container(
+        constraints: BoxConstraints.expand(height: 40.0),
+        color: Colors.black,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Center(
+            child: Text(
+              "BlueShirt",
+              style: TextStyle(
+                  color: Colors.white
+              ),
             ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
-  }
+
+Widget ratingItem() =>
+    Align(
+      alignment: Alignment.topRight,
+      child: Container(
+//        constraints: BoxConstraints.tightFor(
+//          width: 80.0,
+//          height: 30.0
+//        ),
+        color: Colors.black,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start, // Default value
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Icon(
+                Icons.star,
+                color: Colors.yellow,
+              ),
+              const SizedBox(width: 8.0,),
+              Text(
+                  "3.6",
+                  style: TextStyle(
+                      color: Colors.white
+                  ),
+                ),
+            ],
+          ),
+        ),
+      ),
+    );
+
+List<Product> _productList()
+{
+
 }
---
+++ lib/model/Product.dart
@@ -0,0 +1,15 @@
+
+
+import 'package:meta/meta.dart';
+
+class Product {
+
+  String name;
+  double rating;
+  String imageUrl;
+  Product(
+      @required this.name,
+      @required this.rating,
+      @required this.imageUrl
+  )
+}
